b74703b17041ee098e997825844158f211cd4a47
gezish <enggezahegn.w@gmail.com>
2023-07-14 15:43:03 +0300

    update on db model

--- notes/admin.py
+++ notes/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 class NoteAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ('title',)
+    
+class ContactsAdmin(admin.ModelAdmin):
+    Contacts_display = ('contact_name', )
 
 admin.site.register(models.Notes, NoteAdmin)
+admin.site.register(models.Contacts, ContactsAdmin)
--- notes/models.py
+++ notes/models.py
@@ -6,3 +6,8 @@ class Notes(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created = models.DateField(auto_now_add= True)
+    
+class Contacts(models.Model):
+    contact_name = models.CharField(max_length=200)
+    Contact_num = models.IntegerField()
+    created = models.DateField(auto_now_add= True)
